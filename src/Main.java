e65ba4eaf91649d6aa39542f4ff336984f3bdcad
goodtimeswithpaul <moi@paultissot.ch>
2024-01-25 17:12:49 +0100

    feat: added the lic functions signatures for #19
    Co-authored-by: annelibogren <20abogren@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,66 @@ public class Main {
     boolean[][] prelimUnlockMatrix = new boolean[VECTOR_SIZE][VECTOR_SIZE];
     boolean[] finalUnlockVector = new boolean[VECTOR_SIZE];
 
+    public boolean lic0holds() {
+        return false;
+    }
+
+    public boolean lic1holds() {
+        return false;
+    }
+
+    public boolean lic2holds() {
+        return false;
+    }
+
+    public boolean lic3holds() {
+        return false;
+    }
+
+    public boolean lic4holds() {
+        return false;
+    }
+
+    public boolean lic5holds() {
+        return false;
+    }
+
+    public boolean lic6holds() {
+        return false;
+    }
+
+    public boolean lic7holds() {
+        return false;
+    }
+
+    public boolean lic8holds() {
+        return false;
+    }
+
+    public boolean lic9holds() {
+        return false;
+    }
+
+    public boolean lic10holds() {
+        return false;
+    }
+
+    public boolean lic11holds() {
+        return false;
+    }
+
+    public boolean lic12holds() {
+        return false;
+    }
+
+    public boolean lic13holds() {
+        return false;
+    }
+
+    public boolean lic14holds() {
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
