401d0c29ae9b465be407fd7a717d8bee21defb48
goodtimeswithpaul <moi@paultissot.ch>
2024-01-25 17:09:55 +0100

    feat: added the variables we will need #19
    Co-authored-by: annelibogren <20abogren@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
+    final int VECTOR_SIZE = 15;
+
+    public enum LogicalConnectors {
+        ANDD,
+        ORR,
+        NOTUSED;
+    }
+
+    // --- Inputs ---
+    int numPoints;
+    int points;
+    // parameters
+    LogicalConnectors[][] logConMatrix = new LogicalConnectors[VECTOR_SIZE][VECTOR_SIZE];
+    boolean[] prelimUnlockVector = new boolean[VECTOR_SIZE];
+
+    // --- Outputs ---
+    boolean launch;
+    boolean[] conditionsMetVector = new boolean[VECTOR_SIZE];
+    boolean[][] prelimUnlockMatrix = new boolean[VECTOR_SIZE][VECTOR_SIZE];
+    boolean[] finalUnlockVector = new boolean[VECTOR_SIZE];
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
