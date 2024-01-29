30beacb8417fce22c1a429a6d62daac9a1982c11
goodtimeswithpaul <moi@paultissot.ch>
2024-01-29 09:38:23 +0100

    feat: made Vector2D a record, and added utility methods #22

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,2 +1,15 @@
-public class Vector2D {
+public record Vector2D(double x, double y) {
+    public Vector2D add(Vector2D that) {
+        return new Vector2D(this.x + that.x, that.x + that.y);
+    }
+
+    public double dotProduct(Vector2D that) {
+        return this.x * that.x + this.y * that.y;
+    }
+
+    public double distance(Vector2D that) {
+        double distX = (that.x - this.x);
+        double distY = (that.y - this.y);
+        return Math.sqrt(distX * distX + distY * distY);
+    }
 }
