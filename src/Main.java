66b01a3d63e00173c5ebc4b32f36591251fca2e8
goodtimeswithpaul <moi@paultissot.ch>
2024-01-30 11:54:47 +0100

    fix: (#25) changed 'static public' to 'public static'

--- src/Main.java
+++ src/Main.java
@@ -22,63 +22,63 @@ public class Main {
     boolean[][] prelimUnlockMatrix = new boolean[VECTOR_SIZE][VECTOR_SIZE];
     boolean[] finalUnlockVector = new boolean[VECTOR_SIZE];
 
-    static public boolean lic0holds(Point2D points, double length1) {
+    public static boolean lic0holds(Point2D points, double length1) {
         return false;
     }
 
-    static public boolean lic1holds(Point2D points, double radius1) {
+    public static boolean lic1holds(Point2D points, double radius1) {
         return false;
     }
 
-    static public boolean lic2holds(Point2D points, double epsilon) {
+    public static boolean lic2holds(Point2D points, double epsilon) {
         return false;
     }
 
-    static public boolean lic3holds(Point2D points, double area1) {
+    public static boolean lic3holds(Point2D points, double area1) {
         return false;
     }
 
-    static public boolean lic4holds(Point2D points, int numPoints, int qPoints, int quads) {
+    public static boolean lic4holds(Point2D points, int numPoints, int qPoints, int quads) {
         return false;
     }
 
-    static public boolean lic5holds(Point2D points) {
+    public static boolean lic5holds(Point2D points) {
         return false;
     }
 
-    static public boolean lic6holds(Point2D points, int numPoints, int nPoints, double dist) {
+    public static boolean lic6holds(Point2D points, int numPoints, int nPoints, double dist) {
         return false;
     }
 
-    static public boolean lic7holds(Point2D points, int numPoints, int kPoints, double length1) {
+    public static boolean lic7holds(Point2D points, int numPoints, int kPoints, double length1) {
         return false;
     }
 
-    static public boolean lic8holds(Point2D points, int numPoints, int aPoints, int bPoints, double radius1) {
+    public static boolean lic8holds(Point2D points, int numPoints, int aPoints, int bPoints, double radius1) {
         return false;
     }
 
-    static public boolean lic9holds(Point2D points, int numPoints, int cPoints, int dPoints, double epsilon) {
+    public static boolean lic9holds(Point2D points, int numPoints, int cPoints, int dPoints, double epsilon) {
         return false;
     }
 
-    static public boolean lic10holds(Point2D points, int numPoints, int ePoints, int fPoints, double area1) {
+    public static boolean lic10holds(Point2D points, int numPoints, int ePoints, int fPoints, double area1) {
         return false;
     }
 
-    static public boolean lic11holds(Point2D points, int numPoints, int gPoints) {
+    public static boolean lic11holds(Point2D points, int numPoints, int gPoints) {
         return false;
     }
 
-    static public boolean lic12holds(Point2D points, int numPoints, int kPoints, double length1, double length2) {
+    public static boolean lic12holds(Point2D points, int numPoints, int kPoints, double length1, double length2) {
         return false;
     }
 
-    static public boolean lic13holds(Point2D points, int numPoints, int aPoints, int bPoints, double radius1, double radius2) {
+    public static boolean lic13holds(Point2D points, int numPoints, int aPoints, int bPoints, double radius1, double radius2) {
         return false;
     }
 
-    static public boolean lic14holds(Point2D points, int numPoints, int ePoints, int fPoints, double area1, double area2) {
+    public static boolean lic14holds(Point2D points, int numPoints, int ePoints, int fPoints, double area1, double area2) {
         return false;
     }
 
