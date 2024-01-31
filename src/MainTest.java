7d5869aacf179e696d5a627bef08242ef39ecc7c
goodtimeswithpaul <moi@paultissot.ch>
2024-01-31 19:21:02 +0100

    feat: (#13) wrote a test for lic12holds

--- src/MainTest.java
+++ src/MainTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
 
+import java.awt.geom.Point2D;
 import java.util.*;
 
 // Test Class!
@@ -28,5 +29,19 @@ public class MainTest {
     @Test
     public void testExample() {
         assertTrue(true);
+    }
+    @Test
+    public void testLIC12() {
+        Point2D[] points1 = {new Point2D.Double(0,0),
+                new Point2D.Double(0,1),
+                new Point2D.Double(0,0),
+                new Point2D.Double(0,4),
+                new Point2D.Double(0,0),
+                new Point2D.Double(0,10)};
+
+        assertTrue(Main.lic12holds(points1, 1, 5, 4));
+        assertTrue(Main.lic12holds(points1, 2, 5, 4));
+        assertTrue(Main.lic12holds(points1, 3, 5, 4));
+        assertFalse(Main.lic12holds(points1, 4, 5, 4));
     }
 }
