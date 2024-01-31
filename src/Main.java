e67c0da6eb621f40c0e3418f6e9ea5c37f0e6db1
goodtimeswithpaul <moi@paultissot.ch>
2024-01-31 14:49:37 +0100

    feat: (#8) wrote lic7holds

--- src/Main.java
+++ src/Main.java
@@ -188,7 +188,25 @@ public class Main {
         return false;
     }
 
-    public static boolean lic7holds(Point2D[] points, int numPoints, int kPoints, double length1) {
+    public static boolean lic7holds(Point2D[] points, int kPoints, double length1) {
+        if (points == null) {
+            throw new IllegalArgumentException("points is null.");
+        }
+
+        if (points.length < Math.max(3, (kPoints + 2))) {
+            return false;
+        }
+
+        if (!(1 <= kPoints && kPoints <= points.length - 2) || !(length1 >= 0)) {
+            throw new IllegalArgumentException("kPoints is invalid.");
+        }
+
+        for (int i = 0; i < points.length - kPoints - 1; i++) {
+            if (points[i].distance(points[i + kPoints + 1]) > length1) {
+                return true;
+            }
+        }
+
         return false;
     }
 
