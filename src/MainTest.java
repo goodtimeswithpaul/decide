7b1b860f9fa3a9e8bbe5d29eef274b36fad9c3e2
goodtimeswithpaul <moi@paultissot.ch>
2024-01-31 14:01:41 +0100

    feat: (#6) wrote a test for lic5holds

--- src/MainTest.java
+++ src/MainTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
 
+import java.awt.geom.Point2D;
 import java.util.*;
 
 // Test Class!
@@ -29,4 +30,31 @@ public class MainTest {
     public void testExample() {
         assertTrue(true);
     }
+
+    @Test
+    public void testLIC5() {
+        Point2D[] points1 = {new Point2D.Double(0, 0),
+                            new Point2D.Double(0, 1),
+                            new Point2D.Double(0, 5)};
+
+        Point2D[] points2 = {new Point2D.Double(0, 0),
+                            new Point2D.Double(65, 1),
+                            new Point2D.Double(5, 5)};
+
+        Point2D[] points3 = {new Point2D.Double(1000, 1000)};
+
+        Point2D[] points4 = {new Point2D.Double(0, 0),
+                            new Point2D.Double(5, 1),
+                            new Point2D.Double(45, 5)};
+
+
+        assertFalse(Main.lic5holds(points1));
+        assertTrue(Main.lic5holds(points2));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Main.lic5holds(points3);
+        });
+        assertFalse(Main.lic5holds(points4));
+    }
+
+
 }
