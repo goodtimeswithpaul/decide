2835194cb10fd3a37da1a0bef0a413f20a4e11a8
Anneli Bogren <annbog@kth.se>
2024-01-31 22:14:40 +0100

    Add LIC8 function and test Closes #9

--- src/MainTest.java
+++ src/MainTest.java
@@ -119,4 +119,21 @@ public class MainTest {
         assertFalse(Main.lic7holds(points2, 8, 0.5));
 
     }
+
+    @Test
+    public void testLIC8() {
+        Point2D[] testPoints = {new Point2D.Double(0,1),
+            new Point2D.Double(5,5),
+            new Point2D.Double(6,1),
+            new Point2D.Double(1,0),
+            new Point2D.Double(3,4),
+            new Point2D.Double(8,1),
+            new Point2D.Double(1,1)};
+        // Positive Test
+        assertTrue(Main.lic8holds(testPoints, 7, 2, 2,0.5));
+        // Negative Test
+        assertFalse(Main.lic8holds(testPoints, 7, 2, 2,1));
+        // Negative Test
+        assertFalse(Main.lic8holds(testPoints, 7, 5, 3, 0.5));
+    }
 }
